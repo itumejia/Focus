4da7b1d3402002083083cdb72a4bc601054a8acc
Ituriel Mejia <iturielmejia@Ituriels-MacBook-Air.local>
2021-08-25 10:24:11 -0500

    Initial commit

---
+++ Focus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Focus
+//
+//  Created by Ituriel Mejia on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
