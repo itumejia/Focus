f284cb86217e54d07dc8b183bde41f504db0fd12
David Zarate Lopez <A01329785@itesm.mx>
2021-09-09 13:10:25 -0400

    Lista pantalla instrucciones base

---
+++ Focus/HowToViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HowToViewController.swift
+//  Focus
+//
+//  Created by user197888 on 9/7/21.
+//
+
+import Foundation
+import UIKit
+
+
+class HowToViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
