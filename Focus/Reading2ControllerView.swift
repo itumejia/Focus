784e24a42cc63641fc683fd88c5d4ceccf3e942a
Karen <A01733228@itesm.mx>
2021-10-10 03:14:08 -0500

    view controller navigating and added more phrases

--- Focus/Reading2ControllerView.swift
+++ Focus/Reading2ControllerView.swift
@@ -10,21 +10,73 @@ import UIKit
 
 class Reading2ControllerView: UIViewController {
     
-    
+
     @IBOutlet weak var img1: UIImageView!
     @IBOutlet weak var img2: UIImageView!
+    @IBOutlet weak var nivel: UILabel!
     
-    @IBOutlet weak var level: UILabel!
     
-    
-    let images = [UIImage(named: "cars"),  UIImage(named: "cars2")]
-    
+    let images = [
+        UIImage(named: "enredados"), //Enredados
+        UIImage(named: "enredados2"),
+        UIImage(named: "cars"), //Cars
+        UIImage(named: "cars2"),
+        UIImage(named: "rat"), //Ratatouille
+        UIImage(named: "rat2"),
+        UIImage(named: "moana"), //Moana
+        UIImage(named: "moana2"),
+        UIImage(named: "luca"), //Luca
+        UIImage(named: "luca2"),
+        UIImage(named: "toystory"), //Toy Story
+        UIImage(named: "toystory2"),
+        UIImage(named: "monsters"), //Monsters Inc
+        UIImage(named: "monsters2"),
+        UIImage(named: "valiente"), //Valiente
+        UIImage(named: "valiente2"),
+        UIImage(named: "ralph"), //Ralph el demoledor
+        UIImage(named: "ralph2"),
+        UIImage(named: "frozen"), //Valiente
+        UIImage(named: "frozen2"),
+        
+        
+    ]
+     let imagesHighLevels = [
+        UIImage(named: "leon"), //El rey león
+        UIImage(named: "leon2"),
+        UIImage(named: "nemo"), //Nemo
+        UIImage(named: "nemo2"),
+     ]
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        img1.image = images.randomElement()!
+        enredados()
         
     }
+    
+    func enredados(){
+        img1.image = images[0]
+        img2.image = images[1]
+        
+    }
+    
+    
+
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        nivel.text = "Nooo jalaaaa"
+    }
+    
+    
+
+//        let vc = storyboard? .instantiateViewController(identifier: "reading") as! ReadingViewController
+//        vc.phraseToPass = { text in
+//            self.nivel.text = text
+//        }
+//        vc.modalPresentationStyle = .fullScreen
+//        present(vc, animated: true)
+//    }
+
+    
+    
 
 }
--- Focus/ReadingViewController.swift
+++ Focus/ReadingViewController.swift
@@ -9,19 +9,36 @@ import UIKit
 
 class ReadingViewController: UIViewController {
     
-    
-    @IBOutlet weak var startGame: UIButton!
     @IBOutlet weak var phrase: UILabel!
     
-    let phrases = ["La princesa arma un rompecabezas", "El Rayo McQueen corre en una carrera", "Remi el ratón de Ratatouille, cocina una quesadilla", "Mohana está hablando con su abuela"]
     
+    let phrases = ["La princesa arma un rompecabezas", "El Rayo McQueen corre en una carrera", "Remi el ratón de Ratatouille, cocina una quesadilla", "Mohana está hablando con su abuela", "Luca está comiendo", "Buzz y Woody se abrazan", "La pequeña niña Boo espanta al monstruo Sulli", "Valiente practica tiro que con arco", "Ralph el demoledor y Vanellope aprenden a manejar el auto", "Anna y Elsa se dan un abrazo en la nieve"]
+    
+    let phrasesHardLevels = ["El pequeño león Simba baila con Timón y Pumba", "Marlin el pez naranja y Dory el pez azul nadan con la tortugas"]
+    
+    public var phraseToPass: ((String?)->Void)?
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        phrase.text = phrases.randomElement()!
+        phrase.text = phrases.randomElement()! //Elige una frase al azar
         
     }
+    
+
+    @IBAction func startPlaying(_ sender: Any) {
+        //Para pasar de una pantalla a otra
+        let vc = storyboard? .instantiateViewController(identifier: "other") as! Reading2ControllerView
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+        
+        //para pasar la frase de una pantalla a otra
+        phraseToPass?(phrase.text)
+    }
+    
+    
+    
+    
 
 }
 
