42d5c1092679504d2a954f6faf8ddaddc12ba864
David Zarate Lopez <A01329785@itesm.mx>
2021-10-11 19:58:55 -0400

    Board with constant size, needs to be fixed

--- Focus/ZigZagViewController.swift
+++ Focus/ZigZagViewController.swift
@@ -11,10 +11,7 @@ class ZigZagViewController: UIViewController {
     
     var tilesArray = [0,1,2,3,4,5,6,7,8,9]
     
-    @IBOutlet var tiles: [UIButton]!
-    
-    @IBAction func touchingTile(_ sender: UIButton) {
-    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
