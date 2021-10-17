059107e9663b86d028e4211580d4078d01098bae
Karen <A01733228@itesm.mx>
2021-10-16 18:46:33 -0700

    try two

--- Focus/PatternViewController.swift
+++ Focus/PatternViewController.swift
@@ -10,19 +10,22 @@ import UIKit
 
 class PatternViewController: UIViewController {
     var game = ImpostorGame()
-    
-    var different = 0
+    var find: UIImage?
+    var missing = 0
     var shapeSlot = [0,1,2,3,4,5,6]
     var shapeOptions = [0,1,2,3]
+    var randomPatterns = [[1,2,1,2,1],[2,3,2]]
     
     @IBAction func selectShape(_ sender: UIButton) {
-        if sender.tag == different{
+        
+        if sender.tag == missing {
             game.nextLevel()
             level.text = "Nivel " + String(game.level)
             loadImages()
         }
         else{
             level.text = "Perdiste"
+            loadImages()
         }
     }
     
@@ -37,34 +40,99 @@ class PatternViewController: UIViewController {
     }
     
     func loadImages() {
-        var repeated = 0
-        for i in 0...shapes.count - 1 {
-            //var randomButton = Int.random(in: 0...(shapeSlot.count-1))
-            let randomImage = Int.random(in: 0...(shapeOptions.count-1))
-            shapes[i].setImage(UIImage(named: "Shape\(shapeOptions[randomImage])"), for: .normal)
+        restart()
+        missing = Int.random(in: 0...(shapeSlot.count-1))
+            // plantImages.missing(at: randomImage)
+        var position = 0
+        var i = 0
+        let randomPattern = Int.random(in: 0...(randomPatterns.count-1))
+        let randomImage1 = Int.random(in: 0...(shapeOptions.count-1))
+        shapeOptions.remove(at: randomImage1)
+        let randomImage2 = Int.random(in: 0...(shapeOptions.count-1))
+        let firstArray = randomPatterns[randomPattern]
+        print(randomPattern)
+        print(firstArray)
+        while position <= shapeSlot.count-1{
+            print("position\(position)")
+            print(firstArray)
+            if firstArray[i] == 1{
+                shapes[position].setImage(UIImage(named: "Shape\(shapeOptions[randomImage1])"), for: .normal)
+                position += 1
+                i += 1
+              //print()
+//                if position > shapeSlot.count-1{
+//                    break
+//                } else {
+//                    position += 1
+//                }
+//            } else {
+////               for j in 1...firstArray[position]{
+////                    shapes[position].setImage(UIImage(named: "Shape\(shapeOptions[randomImage2])"), for: .normal)
+//                   position += 1
+//                   // print("hay \(j) dos o tres")
+//               }
+//            }
         }
-        
+            else{
+                for _ in 1...firstArray[i]{
+                shapes[position].setImage(UIImage(named: "Shape\(shapeOptions[randomImage2])"), for: .normal)
+                position += 1
+                }
+                i += 1
+            }
+        }
         //Opciones de respuesta (siempre son las mismas)
-        for i in 0...answer.count - 1 {
-            answer[i].setImage(UIImage(named: "Shape\(shapeOptions[i])"), for: .normal)
+        for k in 0...answer.count - 1 {
+            shapeOptions = [0,1,2,3]
+            answer[k].setImage(UIImage(named: "Shape\(shapeOptions[k])"), for: .normal)
         }
+    }
+    
+    func restart(){
         
-            
-//            answer[shapeSlot[randomButton]].setImage(UIImage(named: "Shape\(shapeOptions[randomImage])"), for: .normal)
-//            shapeSlot.remove(at: randomButton)
-//            randomButton = Int.random(in: 0...(shapeSlot.count-1))
-//            answer[shapeSlot[randomButton]].setImage(UIImage(named: "Shape\(shapeOptions[randomImage])"), for: .normal)
-//            repeated = shapeOptions[randomImage]
-//            shapeSlot.remove(at: randomButton)
-//            shapeOptions.remove(at: randomImage)
+        let shapeSlot = [0,1,2,3,4,5,6]
+        let shapeOptions = [0,1,2,3]
+    }
 
         
-        
-//        answer[shapeSlot[0]].setImage(UIImage(named: "Shape\(repeated)"), for: .normal)
-//        shapeSlot.remove(at: 0)
-//        answer[shapeSlot[0]].setImage(UIImage(named: "Shape\(shapeOptions[0])"), for: .normal)
-//        different = shapeSlot[0]
-        
-    }
+
         
 }
+
+//restart()
+//let missing = Int.random(in: 0...(shapeOptions.count-1))
+//    // plantImages.missing(at: randomImage)
+//var position = 0
+//let randomPattern = 1 //Int.random(in: 0...(randomPatterns.count-1))
+//let randomImage1 = Int.random(in: 0...(shapeOptions.count-1))
+//shapeOptions.missing(at: randomImage1)
+//let randomImage2 = Int.random(in: 0...(shapeOptions.count-1))
+//if randomPattern == 1 {
+//    let firstArray = randomPatterns[0]  // ["W", "X", "Y", "Z"]
+//    for i in 0...firstArray.count-1{
+//        print(firstArray[i])
+//        if firstArray[i] == 1{
+//            shapes[position].setImage(UIImage(named: "Shape\(shapeOptions[randomImage1])"), for: .normal)
+//            position += 1
+//        } else{
+//            for _ in 0...firstArray[i]-1{
+//            shapes[position].setImage(UIImage(named: "Shape\(shapeOptions[randomImage2])"), for: .normal)
+//            position += 1
+//            }
+//        }
+//
+//    }
+//
+//    shapes[position].setImage(UIImage(named: "Shape\(shapeOptions[randomImage2])"), for: .normal)
+//}
+//
+////Opciones de respuesta (siempre son las mismas)
+//for i in 0...answer.count - 1 {
+//    answer[i].setImage(UIImage(named: "Shape\(shapeOptions[i])"), for: .normal)
+//}
+//}
+//
+//func restart(){
+//var shapeSlot = [0,1,2,3,4,5,6]
+//var shapeOptions = [0,1,2,3]
+//}
