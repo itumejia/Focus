7edb97b5a223de9cb70f1b6fee6e489e281b9485
David Zarate Lopez <A01329785@itesm.mx>
2021-10-17 13:30:26 -0400

    changed to MVC

--- Focus/ImpostorGame.swift
+++ Focus/ImpostorGame.swift
@@ -2,7 +2,7 @@
 //  ImpostorGame.swift
 //  Focus
 //
-//  Created by user197888 on 10/3/21.
+//  Created by davidzaratel on 10/3/21.
 //
 
 import Foundation
@@ -11,9 +11,17 @@ import UIKit
 
 class ImpostorGame{
     var level = 1
+    var target = 0
+    var plantButtons = [0,1,2,3,4,5,6,7,8,9,10,11]
+    var plantImages = [1,2,3,4,5,6,7]
     
     func nextLevel(){
         level = level + 1
+    }
+    
+    func initializeArrays(){
+        plantButtons = [0,1,2,3,4,5,6,7,8,9,10,11]
+        plantImages = [1,2,3,4,5,6,7]
     }
     
     
